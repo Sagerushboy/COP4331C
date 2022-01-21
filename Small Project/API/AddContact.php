3870a4642820e959ccf3dc9d82ca4fb865a62956
KyleKinyon <98056201+KyleKinyon@users.noreply.github.com>
2022-01-21 14:29:36 -0500

    Add files via upload

---
+++ Small Project/API/AddContact.php
@@ -0,0 +1,45 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$UserID = $inData["UserID"];
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$PhoneNumber = $inData["PhoneNumber"];
+	$EmailAddress = $inData["EmailAddress"];
+	$HomeAddress = $inData["HomeAddress"];
+	$Birthday = $inData["Birthday"];
+	$Notes = $inData["Notes"];
+	
+	$conn = new mysqli("localhost", "SuperUser", "superPassword", "group25");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserID,FirstName,LastName,PhoneNumber,EmailAddress,HomeAddress,Birthday,Notes) VALUES(?,?,?,?,?,?,?,?)");
+		$stmt->bind_param("isssssss", $UserID, $FirstName, $LastName, $PhoneNumber, $EmailAddress, $HomeAddress, $Birthday, $Notes);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
