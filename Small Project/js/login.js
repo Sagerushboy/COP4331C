ef4f8382cbaf4e83a6efb258f8ac560339c41734
asomwaru <asomwaru01@gmail.com>
2022-01-28 11:30:40 -0500

    Login page is mostly done, need to comment and fix bugs

--- Small Project/js/login.js
+++ Small Project/js/login.js
@@ -1,20 +1,48 @@
 const form = document.getElementById("form");
 const button = document.getElementById("button");
 const formType = document.getElementById("formType");
+let baseUrl = "http://group25.xyz/LAMPAPI";
+let endpoint = "";
 
 const usernameBox = document.getElementById("username");
 const passwordBox = document.getElementById("password");
-const confirmPassword = document.getElementById("confirmPassword");
 
-for (item of formType.getElementsByTagName("h3")) {
-  item.addEventListener("click", (e) => {
-    for (i of formType.getElementsByTagName("h3")) {
-      if (i.className === "active-button") {
-        i.className = "active-button";
-      } else {
-        i.className = "inactive-button";
-      }
+const personalInfo = document.getElementById("userInfo");
+const firstNameBox = document.getElementById("firstName");
+const lastNameBox = document.getElementById("lastName");
+
+function setSignType(type) {
+  const buttons = formType.getElementsByTagName("button");
+  // login
+  if (type) {
+    buttons[0].className = "active-button";
+    buttons[1].className = "inactive-button";
+
+    if (!personalInfo.classList.contains("hidden")) {
+      personalInfo.classList.toggle("hidden");
     }
+
+    endpoint = `${baseUrl}/Login.php`;
+    button.value = "Login";
+  } else {
+    // signup
+    buttons[0].className = "inactive-button";
+    buttons[1].className = "active-button";
+
+    if (personalInfo.classList.contains("hidden")) {
+      personalInfo.classList.toggle("hidden");
+    }
+
+    endpoint = `${baseUrl}/SignUp.php`;
+    button.value = "Signup";
+  }
+}
+
+for (item of formType.getElementsByTagName("button")) {
+  item.addEventListener("click", (e) => {
+    const text = e.target.innerHTML;
+
+    setSignType(text === "Login");
   });
 }
 
@@ -25,27 +53,67 @@ window.onload = (e) => {
   let data = sessionStorage.getItem("cookie");
   if (data === null || data === undefined) {
     console.log("No data");
+    setSignType(false);
   } else {
     console.log("Data found");
     console.log(JSON.parse(data));
 
-    // let element = formType.getElementsByTagName("h3")[0];
-    // element.className = "";
+    setSignType(true);
   }
 };
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  let formStuff = [];
   console.log("Submitted form");
 
-  let call = await fetch(
-    "https://random-data-api.com/api/address/random_address?size=3"
-  );
+  if (button.value.toLowerCase() === "login") {
+    if (usernameBox.value.trim() === "" || passwordBox.value.trim() === "") {
+      console.log("User/pass is empty");
+      return;
+    }
+  } else {
+    let empty = [
+      usernameBox.value,
+      passwordBox.value,
+      firstNameBox.value,
+      lastNameBox.value,
+    ].every((item) => item.trim() === "");
 
-  let data = await call.json();
-  formStuff = data.map((item) => item.city);
+    if (empty) {
+      console.log("Empty item(s) in form");
+      return;
+    }
+  }
 
-  sessionStorage.setItem("cookie", JSON.stringify(formStuff));
+  let body = {
+    UserName: usernameBox.value,
+    Password: passwordBox.value,
+    FirstName: firstNameBox.value,
+    LastName: lastNameBox.value,
+  };
+
+  console.log(endpoint);
+
+  let temp = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  console.log(await temp.json());
+
+  // let call = await fetch(endpoint, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(body),
+  // });
+
+  // let data = await call.json();
+  // console.log(data);
+
+  // sessionStorage.setItem("cookie", JSON.stringify(formStuff));
 });
