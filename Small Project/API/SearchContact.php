ca8b3d38e72b5be8b3686ad1ce77bfb0c95cfdab
mif5000 <54731438+mif5000@users.noreply.github.com>
2022-01-29 04:29:55 -0500

    Add files via upload

---
+++ Small Project/API/SearchContact.php
@@ -0,0 +1,70 @@
+<?php
+
+	$inData = getRequestInfo();
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "SuperUser", "superPassword", "group25");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT FirstName,LastName FROM Contacts WHERE FirstName OR LastName like ? and FirstName = ? OR LastName = ?");
+		$userName = "%" . $inData["search"] . "%";
+		$stmt->bind_param("sss", $userName, $inData["FirstName"], $inData["LastName"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["FirstName"] . " " . $row["LastName"] . '"';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
