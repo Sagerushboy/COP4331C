ee128a2afd1489cef251e8a0156d4dfc3b8204f2
asomwaru <asomwaru01@gmail.com>
2022-02-27 22:32:46 -0500

    Some more boilerplate while Atlas is loading

---
+++ MERN Cards/backend/README.md
@@ -0,0 +1,29 @@
+# Backend
+
+## Before Running
+
+Make sure to run `npm install -D` so that you install of the packages including the dev dependencies.
+
+After doing that, you will need to create a file titled `.env` in the same folder that you have the `package-lock.json` file.
+
+Here's what must be put in the file:
+
+```
+USERNAME={db_username}
+PASSWORD={password_to_db}
+PORT={db_port}
+ADDRESS={address_of_db}
+DATABASE={db_name}
+```
+
+This will allow for the backend application to use those variables custom variables when connecting to the database.
+
+## Running
+
+Run the command `npm run dev` to run the server in development mode.
+You should see it say this for the backend application to be working:
+
+```
+Server running on port 8080
+Mongodb connected
+```
--- MERN Cards/backend/src/index.ts
+++ MERN Cards/backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from 'cors';
-// import { connect, } from "mongoose";
+import { connect, } from "mongoose";
 
 const env = dotenv.config();
 
@@ -10,21 +10,21 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
-// const mongoURI: string =
-// 	`mongodb://${process.env.ADDRESS}:${process.env.PORT}`;
+const mongoURI: string =
+	`mongodb://${process.env.ADDRESS}:${process.env.PORT}`;
 
-// connect(mongoURI, {
-// 	auth: {
-// 		username: process.env.USERNAME,
-// 		password: process.env.PASSWORD
-// 	},
-// 	authSource: "admin",
-// 	dbName: process.env.DATABASE,
-// })
-// 	.then(() => console.log("Mongodb connected"))
-// 	.catch((err) => {
-// 		console.log("Error with db")
-// 		console.error(err)
-// 	});
+connect(mongoURI, {
+	auth: {
+		username: process.env.USERNAME,
+		password: process.env.PASSWORD
+	},
+	authSource: "admin",
+	dbName: process.env.DATABASE,
+})
+	.then(() => console.log("Mongodb connected"))
+	.catch((err) => {
+		console.log("Error with db")
+		console.error(err)
+	});
 
 app.listen(8080, () => console.log("Server running on port 8080"));
